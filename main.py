373ae6dc4f017cfa7f38b1d1256f9bf99311990b
SouzaMth <105978404+SouzaMth@users.noreply.github.com>
2022-10-20 01:29:50 +0000

    Aula 1 - 19/10

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ idade = 21
 
 print(nome)
 
+print("Minha idade é",idade)
+print("Minha idade é "+str(idade))
+print(f"Minha idade é {idade} \n")
+print("Minha idade é {}".format(idade))
+
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
 ''' 
 Comentario em bloco
 
